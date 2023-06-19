2bd80770a2ea4312717bbe18bc43a012cab56882
AlexStorm <info@alexstorm.ru>
2023-06-19 12:48:16 +0530

    refactor modules

---
+++ src/app/entry.module.ts
@@ -0,0 +1,14 @@
+
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+  ],
+  providers: [],
+})
+export class RemoteEntryModule {}
